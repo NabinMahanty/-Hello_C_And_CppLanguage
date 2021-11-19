5c9f4cf4472978f31d5e4cf05a1c249986413d07
nabinj8829 <94551785+nabinj8829@users.noreply.github.com>
2021-11-19 21:35:37 +0530

    Create sizeOf_variables.c

---
+++ sizeOf_variables.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+  printf("The size of int = %d byte\n",sizeof(int));
+  printf("The size of float = %d byte\n",sizeof(float));
+  printf("The size of double = %d byte\n",sizeof(double));
+  printf("The size of char = %d byte\n",sizeof(char));
+  printf("The size of long = %d byte\n",sizeof(long));
+  printf("The size of long double = %d byte\n",sizeof(long double));
+   
+}	
