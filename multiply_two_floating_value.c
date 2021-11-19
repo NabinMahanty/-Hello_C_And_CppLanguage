49ff4c56f95c9ec921487ff5dc0931bced8016ce
nabinj8829 <94551785+nabinj8829@users.noreply.github.com>
2021-11-19 20:23:53 +0530

    Create multiply_two_floating_value.c

---
+++ multiply_two_floating_value.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+  float i,j,k;
+  printf("Enter the first value :-");
+  scanf("%f",&i);
+  printf("Enter the sceond value :-");
+  scanf("%f",&j);
+  k=i*j;
+  printf("%f",k);
+}	
