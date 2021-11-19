8699d1477cb87b5588f044c832eba8bcfce009d8
nabinj8829 <94551785+nabinj8829@users.noreply.github.com>
2021-11-19 20:12:48 +0530

    Create add_two__integer.c

---
+++ add_two__integer.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+  int i,j,k;
+  printf("Enter the first value :-");
+  scanf("%d",&i);
+  printf("Enter the sceond value :-");
+  scanf("%d",&j);
+  k=i+j;
+  printf("%d",k);
+}	
