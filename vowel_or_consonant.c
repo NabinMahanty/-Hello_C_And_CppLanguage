b051490c2cbda8498ce15fd6adf78d2887c13e5c
nabinj8829 <94551785+nabinj8829@users.noreply.github.com>
2021-11-20 20:35:02 +0530

    Create vowel_or_consonant.c

---
+++ vowel_or_consonant.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+char c ;
+  printf("Enter the alphabet:\n");
+scanf("%c",&c);
+     if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U')
+  printf("%c is a vowel\n",c);
+ else
+   printf("%c is a consonant\n",c);
+}  
