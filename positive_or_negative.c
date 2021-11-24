ba1d5cd7c81eb57b2694323e43cc23d20c65125e
nabinj8829 <94551785+nabinj8829@users.noreply.github.com>
2021-11-24 19:30:35 +0530

    Create positive_or_negative.c

---
+++ positive_or_negative.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int num;
+	printf("Enter the number:");
+	scanf("%d",&num);
+	if (num<=0)
+	{ if (num==0)
+	printf("You enter o");
+	 else 
+	 printf("This is negativ number"); 
+	}
+	else
+	{
+	printf("This is positive number");
+	}
+}
