e8c074d20a0dc2ab638b40d925073897e899fabe
Nabin MAHANTY <94551785+NabinMahanty@users.noreply.github.com>
2022-01-20 18:24:14 +0530

    Create bist_routine.c

---
+++ bist_routine.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+void main()
+{
+	printf("\t    Bengal Instute of Science & Techonology, Purulia\n");
+	printf("\t\t\t Session 2021-2022\n");
+	printf("\t\t\t   (BCA SEM I)\n");
+	printf("\t _______________________________________________________\n");
+	printf("\t|Time-11:00-11:40|11:50-12:30|12:40-01:20|  |01:30-02:10|\n");
+	printf("\t|________________|___________|___________|  |___________|\n");
+	printf("\t|Mon-  MATH(KK)  |  EVS(BM)  |   FC(RC)  |  |  C(AR)    |\n");
+	printf("\t|________________|___________|___________|  |___________|\n");
+	printf("\t|Tue-  MATH(KK)  |  C(AR)    |   FC(RC)  |  |           |\n");
+	printf("\t|________________|___________|___________|  |___________|\n");
+	printf("\t|Wed-  C(AR)     |  FC(RC)   |  MATH(KK) |  |  EVS(BM)  |\n");
+	printf("\t|________________|___________|___________|  |___________|\n");
+	printf("\t|Thu-  FC(RC)    |  MATH(KK) |   C(AR)   |  |           |\n");
+	printf("\t|________________|___________|___________|  |___________|\n");
+	printf("\t|Fri-  C(AR)     |  FC(RC)   |  MATH(KK) |  |  EVS(BM)  |\n");
+	printf("\t|________________|___________|___________|  |___________|\n");
+	printf("\t|Sat-  MATH(KK)  |  EVS(BM)  |   C(AR)   |  |  FC(RC)   |\n");
+	printf("\t|________________|___________|___________|__|___________|\n");
+}
