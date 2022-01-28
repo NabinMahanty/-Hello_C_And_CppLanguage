ffcec507c501f85658b014ee2ac750f6de6ee565
Nabin Mahanty <94551785+NabinMahanty@users.noreply.github.com>
2022-01-28 18:48:56 +0530

    Create terminate_abnormal_usingAbort().c

---
+++ terminate_abnormal_usingAbort().c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char const *argv[]) {
+/* code */void showMsg()
+{
+  printf("hello good morning\n" );
+}
+printf("Hello sweetie\n");
+abort();
+printf("good morning\n");
+  return 0;
+}
