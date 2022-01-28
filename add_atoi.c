e5f9f0b125c44b1a5f9e6090c0e557b408c1083d
Nabin Mahanty <94551785+NabinMahanty@users.noreply.github.com>
2022-01-28 12:15:30 +0530

    Create add_atoi.c

---
+++ add_atoi.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char const *argv[]) {
+char ch[] = "12";
+char ch1[] = "08";
+int x;
+int y;
+x = atoi(ch);
+y = atoi(ch1);
+printf("%d\n",x+y );
+  /* code */
+  return 0;
+}
