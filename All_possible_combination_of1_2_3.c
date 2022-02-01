275f94af1b30ef9fbec0cc2ede54dca59501b87c
Nabin Mahanty <94551785+NabinMahanty@users.noreply.github.com>
2022-02-01 14:36:23 +0530

    Create All_possible_combination_of1_2_3.c

---
+++ All_possible_combination_of1_2_3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(int argc, char const *argv[]) {
+  int i = 1,j = 1,k = 1;
+  for ( i = 1; i <=3; i++)
+  {
+      for ( j = 1; j <=3; j++)
+      {
+            for ( k = 1; k <=3; k++)
+             {
+                printf("%d%d%d\n",i,j,k);
+            }
+      }
+  }
+  return 0;
+}
